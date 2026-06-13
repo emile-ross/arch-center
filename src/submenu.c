1e61ca8d6f9e25b88b19625bd30cdd124dc3533c
emile-ross <emileross@tutanota.de>
2026-06-13 02:38:46 -0400

    fix: invalid function declaration
    also fixes the various indenting issues

--- src/submenu.c
+++ src/submenu.c
@@ -3,285 +3,290 @@
 
 int system_monitoring(void)
 {
-    int choice = -1;
-    int highlight = 0;
-
-    while(1)
-    {
-	const char *choices[] = 
-    	{
-    	    "htop",
-    	    "btop",
-    	    "glances",
-    	    "Back",
-    	};
-
-    	clear();
-
-    	mvprintw(0, 2, "View system activity using one of the following programs");
-	menu_tip("sub", 2);
-
-    	int n_choices = ARRAY_SIZE(choices);
-    	for (int i = 0; i < n_choices; i++) 
-    	{
-    	    if (i == highlight)
-    	    {
-    	        attron(A_REVERSE);
-    	        mvprintw(3 + i, opt_indent_left + 1, "%s", choices[i]);
-    	        attroff(A_REVERSE);
-    	    }
-    	    else 
-    	    {
-    	        mvprintw(3 + i, opt_indent_left, "%s", choices[i]);
-    	    }
-    	}
-
-    	int c = getch();
-
-	switch (c) 
-    	{
-    	    case KEY_UP:
-    	        highlight--;
-    	        if (highlight < 0)
-    	            highlight = n_choices - 1;
-    	        break;
-
-    	    case KEY_DOWN:
-    	        highlight++;
-    	        if (highlight >= n_choices)
-    	            highlight = 0;
-    	        break;
-
-    	    case 10:  // Enter key
-    	        choice = highlight;
-    	        break;
-
-    	    case 'b':
-    	        endwin();
-		return 0;
-    	}
-
-	char cmd[16];
-	switch (choice)
+	int choice = -1;
+	int highlight = 0;
+	
+	while(1)
 	{
-	    case 0:
-		snprintf(cmd, sizeof(cmd),
-			"htop");
-		system(cmd);
+		const char *choices[] = 
+		{
+			"htop",
+			"btop",
+			"glances",
+			"Back",
+		};
+	
+		clear();
+	
+		mvprintw(0, 2, "View system activity using one of the following programs");
+		menu_tip("sub", 2);
+	
+		int n_choices = ARRAY_SIZE(choices);
+		for (int i = 0; i < n_choices; i++) 
+		{
+			if (i == highlight)
+			{
+				attron(A_REVERSE);
+				mvprintw(3 + i, opt_indent_left + 1, "%s", choices[i]);
+				attroff(A_REVERSE);
+			}
+			else 
+			{
+				mvprintw(3 + i, opt_indent_left, "%s", choices[i]);
+			}
+		}
+	
+		int c = getch();
+		switch (c) 
+		{
+		case KEY_UP:
+			highlight--;
+			if (highlight < 0)
+			{
+				highlight = n_choices - 1;
+			}
+			break;
 
-		choice = -1;
-		highlight = 0;
+		case KEY_DOWN:
+			highlight++;
+			if (highlight >= n_choices)
+			{
+				highlight = 0;
+			}
+			break;
 
-		break;
-	    case 1:
-		snprintf(cmd, sizeof(cmd),
-			"btop");
-		system(cmd);
+		case 10:  /* Enter key */
+			  choice = highlight;
+			  break;
 
-		choice = -1;
-		highlight = 0;
-
-		break;
-	    case 2:
-		snprintf(cmd, sizeof(cmd),
-			"glances");
-		system(cmd);
-
-		choice = -1;
-		highlight = 0;
-
-		break;
-
-	    case 3:
-		endwin();
-	    	return 0;
-	    default:
-		break;
+		case 'b':
+			  endwin();
+			  return 0;
+		}
+	
+		char cmd[16];
+		switch (choice)
+		{
+		case 0:
+			snprintf(cmd, sizeof(cmd),
+					"htop");
+			system(cmd);
+			
+			choice = -1;
+			highlight = 0;
+			
+			break;
+		case 1:
+			snprintf(cmd, sizeof(cmd),
+					"btop");
+			system(cmd);
+		
+			choice = -1;
+			highlight = 0;
+		
+			break;
+		case 2:
+			snprintf(cmd, sizeof(cmd),
+					"glances");
+			system(cmd);
+			
+			choice = -1;
+			highlight = 0;
+		
+			break;
+		
+		case 3:
+			endwin();
+			return 0;
+		default:
+			break;
+		}
 	}
-    }
-    endwin();
-    return 0;
+	endwin();
+	return 0;
 }
 
 int settings_menu(void)
 {
-    int highlight = 0;
-    int choice = -1;
-
-    while(1)
-    {
-	const char *choices[] = 
-    	{
-    	    "Configure this program",
-    	    "Packages",
-    	    "Configure dotfiles",
-    	    "Back",
-    	};
-    	clear();
-
-    	mvprintw(0, 2, "Settings");
-	menu_tip("sub", 2);
-
-    	int n_choices = ARRAY_SIZE(choices);
-    	for (int i = 0; i < n_choices; i++) 
-    	{
-    	    if (i == highlight)
-    	    {
-    	        attron(A_REVERSE);
-    	        mvprintw(3 + i, opt_indent_left + 1, "%s", choices[i]);
-    	        attroff(A_REVERSE);
-    	    }
-    	    else 
-    	    {
-    	        mvprintw(3 + i, opt_indent_left, "%s", choices[i]);
-    	    }
-    	}
-
-	int c = getch();
-
-    	switch (c) 
-    	{
-    	    case KEY_UP:
-    	        highlight--;
-    	        if (highlight < 0)
-    	            highlight = n_choices - 1;
-    	        break;
-
-    	    case KEY_DOWN:
-    	        highlight++;
-    	        if (highlight >= n_choices)
-    	            highlight = 0;
-    	        break;
-
-    	    case 10:  // Enter key
-    	        choice = highlight;
-    	        break;
-
-    	    case 'b':
-    	        endwin();
-		return 0;
-    	}
-
-	char cmd[32];
-	switch (choice)
+	int highlight = 0;
+	int choice = -1;
+	
+	while(1)
 	{
-	    case 0:
-		highlight = 0;
-		choice = -1;
+		const char *choices[] = 
+		{
+			"Configure this program",
+			"Packages",
+			"Configure dotfiles",
+			"Back",
+		};
+		clear();
+	
+		mvprintw(0, 2, "Settings");
+		menu_tip("sub", 2);
+	
+		int n_choices = ARRAY_SIZE(choices);
+		for (int i = 0; i < n_choices; i++) 
+		{
+			if (i == highlight)
+			{
+				attron(A_REVERSE);
+				mvprintw(3 + i, opt_indent_left + 1, "%s", choices[i]);
+				attroff(A_REVERSE);
+			}
+			else 
+			{
+				mvprintw(3 + i, opt_indent_left, "%s", choices[i]);
+			}
+		}
 
-		break;
-	    case 1:
-		snprintf(cmd, sizeof(cmd),
-			"sudo pacman -Q");
-		system(cmd);
-
-		getch(); // intended blocking behaviour
-
-		highlight = 0;
-		choice = -1;
-
-		break;
-	    case 2:
-		highlight = 0;
-		choice = -1;
-
-		break;
-	    case 3: // back button
-		endwin();
+		int c = getch();
+	
+		switch (c) 
+		{
+		    case KEY_UP:
+		        highlight--;
+		        if (highlight < 0)
+		            highlight = n_choices - 1;
+		        break;
+	
+		    case KEY_DOWN:
+		        highlight++;
+		        if (highlight >= n_choices)
+		            highlight = 0;
+		        break;
+	
+		    case 10:  // Enter key
+		        choice = highlight;
+		        break;
+	
+		    case 'b':
+		        endwin();
 	    	return 0;
-	    default:
-		choice = -1;
-		break;
+		}
+	
+		char cmd[32];
+		switch (choice)
+		{
+		case 0:
+			highlight = 0;
+			choice = -1;
+			
+			break;
+		case 1:
+			snprintf(cmd, sizeof(cmd),
+					"sudo pacman -Q");
+			system(cmd);
+			
+			getch(); /* intended blocking behaviour */
+			
+			highlight = 0;
+			choice = -1;
+			
+			break;
+		case 2:
+			highlight = 0;
+			choice = -1;
+			
+			break;
+		case 3: /* back button */
+			endwin();
+			return 0;
+		default:
+			choice = -1;
+			break;
+		}
 	}
-    }
-    endwin();
-    return 0;
+	endwin();
+	return 0;
 }
-
+	
+	
 int disk_management(void)
 {
-    int highlight = 0;
-    int choice = -1;
-
-    while(1)
-    {
-	const char *choices[] = 
-    	{
-    	    "Manage disk space with Wiper",
-    	    "Gnome Disk Utility",
-    	    "WIP",
-    	    "WIP",
-    	};
-    	clear();
-
-    	mvprintw(0, 2, "Disk management");
-	menu_tip("sub", 2);
-
-    	int n_choices = ARRAY_SIZE(choices);
-    	for (int i = 0; i < n_choices; i++) 
-    	{
-    	    if (i == highlight)
-    	    {
-    	        attron(A_REVERSE);
-    	        mvprintw(3 + i, opt_indent_left + 1, "%s", choices[i]);
-    	        attroff(A_REVERSE);
-    	    }
-    	    else 
-    	    {
-    	        mvprintw(3 + i, opt_indent_left, "%s", choices[i]);
-    	    }
-    	}
-
-	int c = getch();
-
-    	switch (c) 
-    	{
-    	    case KEY_UP:
-    	        highlight--;
-    	        if (highlight < 0)
-    	            highlight = n_choices - 1;
-    	        break;
-
-    	    case KEY_DOWN:
-    	        highlight++;
-    	        if (highlight >= n_choices)
-    	            highlight = 0;
-    	        break;
-
-    	    case 10:  // Enter key
-    	        choice = highlight;
-    	        break;
-
-    	    case 'b':
-    	        endwin();
-		return 0;
-    	}
-
-	switch (choice)
+	int highlight = 0;
+	int choice = -1;
+	
+	while(1)
 	{
-	    case 0:
-		highlight = 0;
-		choice = -1;
+		const char *choices[] = 
+		{
+			"Manage disk space with Wiper",
+			"Gnome Disk Utility",
+			"WIP",
+			"WIP",
+		};
 
-		break;
-	    case 1:
-		highlight = 0;
-		choice = -1;
+		clear();
+	
+		mvprintw(0, 2, "Disk management");
+		menu_tip("sub", 2);
+	
+		int n_choices = ARRAY_SIZE(choices);
+		for (int i = 0; i < n_choices; i++) 
+		{
+			if (i == highlight)
+			{
+				attron(A_REVERSE);
+				mvprintw(3 + i, opt_indent_left + 1, "%s", choices[i]);
+				attroff(A_REVERSE);
+			}
+			else 
+			{
+				mvprintw(3 + i, opt_indent_left, "%s", choices[i]);
+			}
+		}
 
-		break;
-	    case 2:
-		highlight = 0;
-		choice = -1;
-
-		break;
-	    case 3: // back button
-		endwin();
+		int c = getch();
+	
+		switch (c) 
+		{
+		    case KEY_UP:
+		        highlight--;
+		        if (highlight < 0)
+		            highlight = n_choices - 1;
+		        break;
+	
+		    case KEY_DOWN:
+		        highlight++;
+		        if (highlight >= n_choices)
+		            highlight = 0;
+		        break;
+	
+		    case 10:  // Enter key
+		        choice = highlight;
+		        break;
+	
+		    case 'b':
+		        endwin();
 	    	return 0;
-	    default:
-		choice = -1;
-		break;
+		}
+	    
+		switch (choice)
+		{
+		case 0:
+			highlight = 0;
+			choice = -1;
+		
+			break;
+		case 1:
+			highlight = 0;
+			choice = -1;
+		
+			break;
+		case 2:
+			highlight = 0;
+			choice = -1;
+		
+			break;
+		case 3: /* back button */
+			endwin();
+		    	return 0;
+		default:
+			choice = -1;
+			break;
+		}
 	}
-    }
-    endwin();
-    return 0;
+	endwin();
+	return 0;
 }
